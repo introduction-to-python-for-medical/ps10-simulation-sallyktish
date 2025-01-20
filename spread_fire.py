1f48b998e32c31701fc1b966a19e490190135bf1
sallyktish <ktishsally@gmail.com>
2025-01-20 20:28:41 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -10,10 +10,8 @@ def spread_fire(grid):
         for j in range(grid_size):  # Iterate until the last index
             if grid[i][j] == 1:  # If the tree is burning (1)
                 # Check neighbors down and to the right (diagonal neighbors)
-                if i + 1 < grid_size and grid[i+1][j] == 1:  # Check down
-                    update_grid[i+1][j] = 2  # Set the neighbor on fire
+                if (i > 0 and grid[i-1][j]==2) or (i < grid_size - 1 and grid[i+1][j]==2) \ (j > 0 and grid[i][j-1] ==2) or (j < grid_size - 1 and grid[i][j+1]==2):
+                    update_grid[i][j] = 2 
                 
-                if j + 1 < grid_size and grid[i][j+1] == 1:  # Check right
-                    update_grid[i][j+1] = 2  # Set the neighbor on fire
 
     return update_grid
